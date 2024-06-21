751d285d83fae18ab286cffb1113e75d55489155
SurajBhosale003 <surajedu003@gmail.com>
2024-06-21 11:48:11 +0530

    added slice for user and modified the store

--- esignDash/src/redux/store.js
+++ esignDash/src/redux/store.js
@@ -1,23 +1,28 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import userReducer from './reducers/documentReducerSlice';
-import { persistReducer , persistStore  } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import documentReducer from './reducers/documentReducerSlice';
+import userReducer from './reducers/userReducerSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const rootReducer = combineReducers({
+  esign_document: documentReducer,
+  user: userReducer,
+});
 
-const rootReducer = combineReducers({esign_document : userReducer})
-const persistConfig ={
-  key : 'root',
+const persistConfig = {
+  key: 'root',
   storage,
-  version : 1,
-}
-const persistedReducer = persistReducer ( persistConfig , rootReducer)
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer ,
-  middleware: (getDefaultMiddleware) => 
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-    serializableCheck: false,
-  }),
-})
+      serializableCheck: false,
+    }),
+});
 
-export const persistor = persistStore( store )
+export const persistor = persistStore(store);
