3bc6db4501c33b0f26360580bd8b264b81595fdf
SurajBhosale003 <surajedu003@gmail.com>
2024-08-12 16:37:55 +0530

    changed Helper file location

---
+++ esignDash/src/pages/Dashboard/helper/Interface.ts
@@ -0,0 +1,16 @@
+export interface PdfRendererProps {
+    pdfData: string;
+  }
+
+export interface ComponentData {
+    id: number;
+    type: string;
+    content?: string;
+    pageNo: number;
+    value?: string; 
+    position: { top: number; left: number };
+    size: { width: number; height: number };
+    name: string;
+    fontSize?: number;
+    assign?: string[];
+  }
